2b05df11d8e1a25c2f55e931183f1d633093f371
EKM96 <124158593+EKM96@users.noreply.github.com>
2024-02-06 18:00:58 -0600

    refactor: change code to factory function and object litteral patterns

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
-const players = {
-  options: [{user: {}}, {cpu: {}}],  
-};
+function createGame() {
+  const game = {
+    gameboard: {
+      cells: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    },
+    
+    players: {
+      user: {},
+      cpu: {},
+    },
+    
+    logic: {
+      randomStart() {
+        const startingPlayer = getRandom(['user', 'cpu']);
+        
+        if (startingPlayer === 'user') {
+          logMessage('You start');
+        } else if (startingPlayer === 'cpu') {
+          logMessage('CPU starts');
+        }
+      },
+    },
+     
+    init() {
+      game.logic.randomStart();
+    }
+  }
 
-function chooseRandom(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
+  function getRandom(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  };
+
+  function logMessage(message) {
+    console.log(message);
+  };
+
+  game.init();
 }
 
-const startingPlayer = function chooseRandomStartingPlayer() {
-  const players = ['user', 'cpu'];
-  return chooseRandom(players);
-};
+const ticTacToe = createGame();
 
-function logMessage(message) {
-  console.log(message);
-}
-
-const startingMessage = function logStartingPlayerMessage(startingPlayer) {
-  const starts = startingPlayer();
-  
-  if (starts === 'user') {
-    logMessage('You start');
-  } else if (starts === 'cpu') {
-    logMessage('CPU starts');
-  }
-};
-
-const gameboard = {
-  init() {
-    startingMessage(startingPlayer);
-  }
-};
-
-gameboard.init();
-
-
-
+    
