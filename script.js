8dc36fa91e14b7ef809dd368b73a1b131091c8ab
EKM96 <124158593+EKM96@users.noreply.github.com>
2024-02-07 21:15:38 -0600

    feat: add cpu turn logic

--- script.js
+++ script.js
@@ -1,42 +1,51 @@
-function createGame() {
-  const game = {
-    gameboard: {
-      cells: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    },
-    
-    players: {
-      user: {},
-      cpu: {},
-    },
-    
-    logic: {
-      randomStart() {
-        const startingPlayer = getRandom(['user', 'cpu']);
-        
-        if (startingPlayer === 'user') {
-          logMessage('You start');
-        } else if (startingPlayer === 'cpu') {
-          logMessage('CPU starts');
-        }
-      },
-    },
-     
-    init() {
-      game.logic.randomStart();
-    }
+const gameboard = {
+  cells: [1, 2, 3, 4, 5, 6, 7, 8, 9],  
+}
+  
+const user = {};
+  
+const cpu = {
+  turn() {
+    let randomCell = getRandom(gameboard.cells);
+    logMessage(`CPU choses cell: ${randomCell}`);
+    updateActiveCells(gameboard.cells, randomCell);
+    logMessage(`Active Cells: ${gameboard.cells}`);
   }
+};
 
-  function getRandom(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
-  };
+function getRandom(arr) { return arr[Math.floor(Math.random() * arr.length)] };
 
-  function logMessage(message) {
-    console.log(message);
-  };
+function logMessage(message) { console.log(message) };
 
-  game.init();
+function randomStart() {
+  const startingPlayer = getRandom(['user', 'cpu']);
+  
+  if (startingPlayer === 'user') {
+    logMessage('You start');
+  } else if (startingPlayer === 'cpu') {
+    logMessage('CPU starts');
+  }
 }
 
-const ticTacToe = createGame();
-
+function updateActiveCells(arr, item) { arr.splice(arr.indexOf(item), 1) }
     
+function init() {
+  randomStart();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
