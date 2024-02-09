9f10ece1a4220be190f0811241757d69835e48e9
EKM96 <124158593+EKM96@users.noreply.github.com>
2024-02-09 12:19:16 -0600

    feat: add players turn functionality

--- script.js
+++ script.js
@@ -3,18 +3,34 @@ const gameboard = {
 }
   
 const user = {
-  select(num) {
+  turnMessage: '\nIt\'s your turn, select a cell\n', 
+  selectCell(num) {
+    const repeated = searchAgainstActiveCells(num) === false;
 
-  }
+    if (repeated) {
+      logMessage('Select another cell, cell already taken');
+      return;
+  
+    } else {
+      updateActiveCells(gameboard.cells, num);
+      logMessage(`Selected cell: ${num}`);
+      logMessage(`Active cells: ${gameboard.cells}`);
+      logMessage(cpu.turnMessage);
+      setTimeout(cpu.selectCell, 3000);
+    }
+  }, 
 };
+
   
 const cpu = {
-  turn() {
+  turnMessage: '\nIt\'s cpu turn\n',
+  selectCell() {
     let randomCell = getRandom(gameboard.cells);
     logMessage(`CPU choses cell: ${randomCell}`);
     updateActiveCells(gameboard.cells, randomCell);
     logMessage(`Active Cells: ${gameboard.cells}`);
-  }
+    logMessage(user.turnMessage);
+  },
 };
 
 function getRandom(arr) { return arr[Math.floor(Math.random() * arr.length)] };
@@ -22,41 +38,33 @@ function getRandom(arr) { return arr[Math.floor(Math.random() * arr.length)] };
 function logMessage(message) { console.log(message) };
 
 function randomStart() {
-  const startingPlayer = getRandom(['user', 'cpu']);
+  turn = getRandom(['user', 'cpu']);
   
-  if (startingPlayer === 'user') {
-    logMessage('You start');
-  } else if (startingPlayer === 'cpu') {
-    logMessage('CPU starts');
-  }
+  (turn === 'user') ? logMessage('You start') : logMessage('CPU starts');
 }
 
 function updateActiveCells(arr, item) { arr.splice(arr.indexOf(item), 1) }
     
 function searchAgainstActiveCells(cell) { return gameboard.cells.includes(cell) };
 
-function selectCell(num) {
-  const repeated = searchAgainstActiveCells(num) === false;
-  
-  if (repeated) {
-    logMessage('Select another cell, cell already taken');
-    return;
-
-  } else {
-    updateActiveCells(gameboard.cells, num);
-    logMessage(`Selected cell: ${num}`);
-    logMessage(`Active cells: ${gameboard.cells}`);
-  }
-}
+let turn;
 
 function init() {
   randomStart();
+  play();
 }
 
+function play() {
+  if (turn === 'user') {
+    logMessage(user.turnMessage);    
+  }
+  else if (turn === 'cpu') {
+    logMessage(cpu.turnMessage);
+    cpu.selectCell();
+  }
+}
 
-
-
-
+init();
 
 
 
